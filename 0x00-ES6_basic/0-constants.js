857c22d477366546f5edfbda6e2b9d207bfcf5e3
liusseka <cjmatheka@gmail.com>
2024-09-30 19:17:45 +0300

    Correct Task 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,15 @@
+// Task First Function
 export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
 }
 
+// Get Last function
 export function getLast() {
   return " is okay";
 }
 
+// Task Next Function
 export function taskNext() {
   let combination = "But sometimes let";
   combination += getLast();

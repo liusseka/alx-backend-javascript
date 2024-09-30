e4c0cc61f678d7a281f1ebaa6a73b00799e6b5b6
liusseka <cjmatheka@gmail.com>
2024-09-30 20:09:14 +0300

    Add Completed Tasks

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,29 @@
-// Task First Function
+/**
+ * Creates a constant and a variable using const and let.
+ *
+ * @returns {string} The task statement.
+ */
 export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
 }
 
-// Get Last function
+/**
+ * Returns a string to be used in combination.
+ *
+ * @returns {string} The string ' is okay'.
+ */
 export function getLast() {
   return " is okay";
 }
 
-// Task Next Function
+/**
+ * Combines two strings to form a complete statement.
+ *
+ * @returns {string} The combined statement.
+ */
 export function taskNext() {
   let combination = "But sometimes let";
   combination += getLast();
-
   return combination;
 }
